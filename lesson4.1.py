3544a65bea5c79d28e6ad532edca3188c648400b
Weraxi <vznkv06@yandex.ru>
2023-07-02 16:44:54 +0300

    Задания 4 и 5 ко 2-ому уроку.

--- lesson4.1.py
+++
@@ -1,7 +0,0 @@
-def palindrome(string):
-    return string.strip() == string[::-1].strip()
-
-if __name__ == "__main__":
-    user_word = input()
-    result = palindrome(user_word)
-    print(result)
